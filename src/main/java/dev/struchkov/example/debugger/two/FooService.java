e9e07c29a8f3579433bdc194eaefd5f012a3ec0a
Struchkov Mark <git@upagge.ru>
2022-02-19 09:02:13 +0300

    Добавил примеры для дебага

--- src/main/java/dev/struchkov/example/debugger/two/FooService.java
+++ src/main/java/dev/struchkov/example/debugger/two/FooService.java
@@ -1,12 +1,19 @@
 package dev.struchkov.example.debugger.two;
 
 import dev.struchkov.example.debugger.box.SurpriseBox;
+import dev.struchkov.example.debugger.box.SurpriseException;
 
 public class FooService {
 
     public void example() {
         for (int i = 0; i < 1000; i++) {
             SurpriseBox.surpriseOne(i);
+
+//            try {
+//                SurpriseBox.surpriseOne(i);
+//            } catch (SurpriseException e) {
+//                System.err.println(e.getMessage());
+//            }
         }
     }
 
