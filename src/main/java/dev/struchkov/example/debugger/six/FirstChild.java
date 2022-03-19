ee743ac984bbeb386416c4605a3095f603a157d2
Struchkov Mark <git@upagge.ru>
2022-03-19 09:39:31 +0300

    Усовершенствование пакета six

--- src/main/java/dev/struchkov/example/debugger/six/FirstChild.java
+++ src/main/java/dev/struchkov/example/debugger/six/FirstChild.java
@@ -5,6 +5,10 @@ public class FirstChild extends AbstractService {
     @Override
     int processing(int arg) {
         final int result = arg + 1;
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
         return result;
     }
 
--- src/main/java/dev/struchkov/example/debugger/six/SecondChild.java
+++ src/main/java/dev/struchkov/example/debugger/six/SecondChild.java
@@ -5,6 +5,10 @@ public class SecondChild extends AbstractService {
     @Override
     int processing(int arg) {
         final int result = arg + 2;
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
         return result;
     }
 
