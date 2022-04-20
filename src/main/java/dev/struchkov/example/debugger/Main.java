90756ccd277396f426152e71f7cb7222c7b6b1ee
Struchkov Mark <mark@struchkov.dev>
2022-04-20 18:16:33 +0300

    Удалил лишний класс

--- src/main/java/dev/struchkov/example/debugger/Main.java
+++
@@ -1,9 +0,0 @@
-package dev.struchkov.example.debugger;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println(0.1 + 0.2); // 0.30000000000000004
-    }
-
-}
